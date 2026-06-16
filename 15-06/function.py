3e7795542a9585955e59610e2de08ba12288f5b2
kadambarishinde32-arch <kadambarishinde32@gmail.com>
2026-06-16 09:29:18 +0530

    fuctions

--- 15-06/function.py
+++ 15-06/function.py
@@ -1,77 +1,77 @@
-# def greet():
-#     print("hello")
-# greet()
+def greet():
+    print("hello")
+greet()
 
-# #fuction to find cube
-# def cube(num):
-#     print(num**3)
-# num=int(input("enter your number:"))
-# cube(5)
+#fuction to find cube
+def cube(num):
+    print(num**3)
+num=int(input("enter your number:"))
+cube(5)
 
-# def getno():
-#     print(10)
+def getno():
+    print(10)
 
-# op=getno()
-# print(op)
+op=getno()
+print(op)
 
-# print(getno())
+print(getno())
 
-# def add(a,b):
-#     return a+b
-# print(add(10,20))
-# print(add(10.5,20.3))
-
-
-# print("start")
-# try:
-#     print(10/0)
-# except ZeroDivisionError:
-#     print("cannot divide by zero")
-
-# print("start")
-# try:
-#     print(10/0)
-# except ZeroDivisionError as e:
-#     print(e)
-
-# try:
-#     a={"name":"ram"}
-#     print(a["name"])
-#     print(a["age"])
-# except Exception as e:
-#     print("key not present")
-# else:
-#     print("end")
-# finally:
-#     print("-------")
+def add(a,b):
+    return a+b
+print(add(10,20))
+print(add(10.5,20.3))
 
 
-# try:
-#     n1=int(input("enter 1st number:"))
+print("start")
+try:
+    print(10/0)
+except ZeroDivisionError:
+    print("cannot divide by zero")
 
-#     n2=int(input("enter 2nd number:"))
+print("start")
+try:
+    print(10/0)
+except ZeroDivisionError as e:
+    print(e)
+
+try:
+    a={"name":"ram"}
+    print(a["name"])
+    print(a["age"])
+except Exception as e:
+    print("key not present")
+else:
+    print("end")
+finally:
+    print("-------")
 
 
-#     def add():
-#          return n1+n2
-#     print(add())
+try:
+    n1=int(input("enter 1st number:"))
 
-#     add()
-
-# except:
-#      print("enter numbers only!!!")
+    n2=int(input("enter 2nd number:"))
 
 
-# class demo:
-#      c_name="xyz"
+    def add():
+         return n1+n2
+    print(add())
 
-#      def __init__(self):
-#         print("default constructor called")
+    add()
 
-# print(demo.c_name)
+except:
+     print("enter numbers only!!!")
 
-# obj=demo()
-# print(obj.c_name)
+
+class demo:
+     c_name="xyz"
+
+     def __init__(self):
+        print("default constructor called")
+
+print(demo.c_name)
+
+obj=demo()
+print(obj.c_name)
 
 class student:
     def __init__(self,name,age):
